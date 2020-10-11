72bebbcbf0a1bf8028954d5bebfdb7305cf3ab36
lemon <lemon_orange_juice@163.com>
2020-10-11 22:39:49 +0800

    first commit

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] E:\\\\project\\\\ToyReact\\\\main.js: .pragma is not a valid Plugin property\\n- Maybe you meant to use\\n\\\"plugin\\\": [\\n  [\\\"@babel/plugin-transform-react-jsx\\\", {\\n  \\\"pragma\\\": \\\"createElement\\\"\\n}]\\n]\\nTo be a valid plugin, its name and options should be wrapped in a pair of brackets\\n    at E:\\\\project\\\\ToyReact\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\plugins.js:65:42\\n    at Array.forEach (<anonymous>)\\n    at validatePluginObject (E:\\\\project\\\\ToyReact\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\validation\\\\plugins.js:54:20)\\n    at E:\\\\project\\\\ToyReact\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:244:55\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (E:\\\\project\\\\ToyReact\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:26:3)\\n    at Generator.next (<anonymous>)\\n    at step (E:\\\\project\\\\ToyReact\\\\node_modules\\\\gensync\\\\index.js:254:32)\\n    at evaluateAsync (E:\\\\project\\\\ToyReact\\\\node_modules\\\\gensync\\\\index.js:284:5)\\n    at Function.errback (E:\\\\project\\\\ToyReact\\\\node_modules\\\\gensync\\\\index.js:108:7)\");\n\n//# sourceURL=webpack://toyreact/./main.js?");
+/******/ })()
+;
