b3170fa18db148b3404af4bc445ffa6fc2301ea7
lemon <lemon_orange_juice@163.com>
2020-10-12 15:21:14 +0800

    lesson 1

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-for (let i of [1,2,3]){
-    console.log(i);
-}
+import {createElement, Component, render} from "./toy-react";
 
-let a = <div/>
+class MyComponent extends Component{
+    render(){
+        return <div>
+            <h1>my component</h1>
+            {this.children}
+        </div>
+    }
+}
+render(<MyComponent id="a" class="c">
+    <div>abc</div>
+    <div></div>
+    <div></div>
+</MyComponent>,document.body);
+
